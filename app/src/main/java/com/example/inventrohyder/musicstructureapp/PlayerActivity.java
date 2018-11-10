0e105548e47c3384828b038258624ddbc5d6a720
Inventrohyder <haitham.hyder@gmail.com>
2018-11-10 09:37:50 -0800

    Feat: Optimize audio playback
    Release the media player before playing an audio file.
    Release the media player when the audio has completed playing.

--- app/src/main/java/com/example/inventrohyder/musicstructureapp/PlayerActivity.java
+++ app/src/main/java/com/example/inventrohyder/musicstructureapp/PlayerActivity.java
@@ -2,8 +2,8 @@ package com.example.inventrohyder.musicstructureapp;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,6 +14,22 @@ public class PlayerActivity extends AppCompatActivity {
 
     // Allows the play button to switch between play and pause icons
     boolean play = true;
+
+    /**
+     * Handles playback of all the sound files
+     */
+    MediaPlayer mMediaPlayer;
+
+    /**
+     * The listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +65,17 @@ public class PlayerActivity extends AppCompatActivity {
         final int playImageID = R.drawable.ic_play_arrow_white_48dp;
         final int pauseImageID = R.drawable.ic_pause_white_48dp;
 
-        // Create the MediaPlayer object to play the audio
-        final MediaPlayer mediaPlayer =  MediaPlayer.create(getApplicationContext(),audioResourceId);
+        // Release the media player if it currently exists because we are about to
+        // play a different sound file.
+        releaseMediaPlayer();
+
+        // Create the MediaPlayer object to play the audio resource associated
+        // with the current podcast
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), audioResourceId);
+
+        // Setup a listener on the media player, so that we can stop and release the
+        // media player once the sounds has finished playing.
+        mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,14 +84,32 @@ public class PlayerActivity extends AppCompatActivity {
                 if (play) {
                     // Set the button image to pause and start playing the audio
                     imageButton.setImageResource(pauseImageID);
-                    mediaPlayer.start();
+                    mMediaPlayer.start();
                 } else {
                     // Set the button image to play and stop playing the audio
                     imageButton.setImageResource(playImageID);
-                    mediaPlayer.stop();
+                    mMediaPlayer.stop();
                 }
                 play = !play;
             }
         });
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
+
 }
